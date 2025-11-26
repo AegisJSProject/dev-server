dd836ede3c61dec4ddb9985163cda4cc4d6e9e30
Chris Zuber <shgysk8zer0@gmail.com>
2025-11-26 10:31:23 -0800

    Add context object debugging to home page
    Expose and display filtered context object in the home page for debugging purposes. Updated index.html to show context details, and bumped version to 1.0.3.

--- home.js
+++ home.js
@@ -2,7 +2,9 @@ import { readFile } from 'node:fs/promises';
 import { imports } from '@shgysk8zer0/importmap';
 import { getCSP, importmap, integrity } from './csp.js';
 
-export default async (req) => {
+const NO_MAP = ['controller', 'socket', 'reject', 'signal', 'resolve'];
+
+export default async (req, context) => {
 	const doc = await readFile(process.cwd() + '/index.html', { encoding: 'utf8' });
 
 	const request = JSON.stringify({
@@ -20,7 +22,15 @@ export default async (req) => {
 			.replaceAll('{{ IMPORTMAP }}', importmap)
 			.replaceAll('{{ INTEGRITY }}', integrity)
 			.replaceAll('{{ POLYFILLS }}', imports['@shgysk8zer0/polyfills'])
-			.replaceAll('{{ REQUEST }}', request),
+			.replaceAll('{{ REQUEST }}', request)
+			.replaceAll('{{ CONTEXT }}', JSON.stringify(
+				Object.fromEntries(Object.keys(context)
+					.filter(key => ! NO_MAP.includes(key))
+					.map(key => [key, context[key]])
+				),
+				null,
+				4
+			)),
 		{
 			headers: {
 				'Content-Type': 'text/html',
